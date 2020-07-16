264c4382406733dbcd1eed19a25fa7a22c126847
Vlado Senasi <vladimir.senasi@nalgoo.com>
2020-07-16 11:22:22 +0200

    fixed fetch.sh for use with long URLs

--- fetch.sh
+++ fetch.sh
@@ -3,7 +3,7 @@
 URL=$1
 
 if [ ! -z "$URL" ]; then
-    curl -sS -w "[%{http_code}] %{url_effective}" -o - $URL 2>&1 | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S] "), $0 }'
+    curl -sS -w "[%{http_code}] %{url_effective}" -o - "$URL" 2>&1 | gawk '{ print strftime("[%Y-%m-%d %H:%M:%S] "), $0 }'
 else
     echo "missing URL argument; use: $0 https://url-to-fetch.com"
 fi
